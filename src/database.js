270fd0af497e3cca9800e7635a084a84166dd68b
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-09 18:40:23 -0500

    update mongoose

--- src/database.js
+++ src/database.js
@@ -3,10 +3,6 @@ const {connect} = require('mongoose')
 const {DB_HOST, DB_PORT, DB_NAME} = process.env
 const DB_URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
-connect(DB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true // habilita unique
-})
+connect(DB_URI)
 	.then(db => console.log('Mongo connected'))
 	.catch(err => console.log(err))
