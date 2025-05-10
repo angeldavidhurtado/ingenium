5b2e6c0f550d556c4a8cfc2a57515ce3746b8abf
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-10 13:40:10 -0500

    db_uri

--- src/database.js
+++ src/database.js
@@ -10,6 +10,10 @@ const DB_URI = DB_URI_SRV
 	? DB_URI_SRV
 	: `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
+console.log('-----------------------------------')
+console.log(DB_URI)
+console.log('-----------------------------------')
+
 connect(DB_URI)
 	.then(db => console.log('Mongo connected'))
 	.catch(err => console.log(err))
