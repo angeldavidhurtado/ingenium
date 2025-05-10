11851be8047381bc693a22122a4d8cbf4d3661eb
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-10 18:40:30 -0500

    login

--- src/server.js
+++ src/server.js
@@ -30,9 +30,11 @@ app.use(session({
   }),
   cookie: {
     maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in ms
+    /*
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
     secure: true
+    */
   }
 }))
 
