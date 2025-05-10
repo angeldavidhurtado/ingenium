a30a498b523b4489ab7c30b0daa7b0b66e200286
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-10 15:41:28 -0500

    production err

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+const mongoStore  = require('connect-mongo')
 const flash = require('connect-flash')
 const helmet = require('helmet');
 const compression = require('compression');
@@ -9,6 +10,32 @@ const passport = require('passport')
 // Initialization
 const app = express()
 require('./config/passport')
+
+const {
+	DB_URI_SRV,
+	DB_HOST,
+	DB_PORT,
+	DB_NAME
+} = process.env
+const DB_URI = DB_URI_SRV
+	? DB_URI_SRV
+	: `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'un-secreto-super-fuerte',
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: DB_URI, // URI of Atlas o local
+    collectionName: 'sessions', // name of collection-data where saved sessions
+    ttl: 14 * 24 * 60 * 60 // 14 days in sec
+  }),
+  cookie: {
+    maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in ms
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true
+  }
+}))
 
 app.use(helmet());
 app.use(compression());
