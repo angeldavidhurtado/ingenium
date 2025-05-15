419bc355cf0427cb77f04a4259b74b53c075105a
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-15 11:51:43 -0500

    cofigurando jest

---
+++ tests/routes/users.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest')
+const app = require('../../app')  // tu archivo express
+const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
+
+let mongoServer
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create()
+  const uri = mongoServer.getUri()
+  await mongoose.connect(uri)
+})
+
+afterAll(async () => {
+  await mongoose.disconnect()
+  await mongoServer.stop()
+})
+
+describe('GET /users', () => {
+  it('debe responder con 200 OK y devolver un array de usuarios', async () => {
+    const response = await request(app).get('/users')
+    expect(response.statusCode).toBe(200)
+    expect(Array.isArray(response.body)).toBe(true)
+  })
+})
